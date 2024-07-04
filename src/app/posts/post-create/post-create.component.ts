75fb1228afe1ed0b1cac17bbf9285b30179aa56f
andreymyqa <andrey@mytime.com>
2024-07-04 10:23:30 -0700

    Controls to Store the image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -28,9 +28,9 @@ export class PostCreateComponent implements OnInit {
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
-      content: new FormControl(null, {
-        validators: [Validators.required]
-      })
+      content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]}),
+
     })
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -47,6 +47,7 @@ export class PostCreateComponent implements OnInit {
           this.form.setValue({
             'title': this.post.title,
             'content': this.post.content,
+            'image': null
           });
         })
       } else {
@@ -55,6 +56,12 @@ export class PostCreateComponent implements OnInit {
       }
     })
 
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+    this.form.patchValue({image: file});
+    this.form.get('image')?.updateValueAndValidity();
   }
 
 
