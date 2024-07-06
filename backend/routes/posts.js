517f8bf7ec5a2cbfa05dfcbb03c80bd8e90fc44b
andreymyqa <andrey@mytime.com>
2024-07-06 15:42:04 -0700

    8-104 Middleware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Post = require("../models/post");
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -26,7 +27,7 @@ const storage = multer.diskStorage({
   }
 })
 
-router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
+router.post("", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
@@ -46,7 +47,7 @@ router.post("", multer({storage: storage}).single("image"), (req, res, next) => 
   });
 });
 
-router.put('/:id', multer({storage: storage}).single("image"),
+router.put('/:id', checkAuth, multer({storage: storage}).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
     if (req.file) {
@@ -105,7 +106,7 @@ router.get("/:id", (req, res, next) => {
   })
 })
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
     res.status(200).json({message: "Post deleted!"});
