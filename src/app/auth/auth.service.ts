40a9f7a0dc09639f0215457feaacb5cec10626ee
andreymyqa <andrey@mytime.com>
2024-07-06 19:54:49 -0700

    8-109 Redirecting Users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {AuthDataModel} from "./auth-data.model";
 import {Subject} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
   private token: string | undefined | null;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   getToken() {
@@ -32,6 +33,7 @@ export class AuthService {
       .subscribe(response => {
         console.log(response);
       })
+    this.router.navigate(['/']);
   }
 
   login(email: string, password: string) {
@@ -43,6 +45,7 @@ export class AuthService {
         if (token) {
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
       })
   }
@@ -51,5 +54,6 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -17,6 +17,7 @@ export class SignupComponent {
     if (form.invalid) {
       return;
     }
+    this.isLoading = true;
     this.authService.createUser(form.value.email, form.value.password)
   }
 
