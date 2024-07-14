e3671e9837ece62e1043ddfc53f830b6dae05de1
andreymyqa <andrey@mytime.com>
2024-07-13 20:40:56 -0700

    11_Optimizations_133

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,17 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
-
-import {MatFormField, MatInput} from '@angular/material/input'
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
-import {MatCard, MatCardFooter} from "@angular/material/card";
-import {MatAnchor, MatButton} from "@angular/material/button";
 import {HeaderComponent} from './header/header/header.component';
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatIcon} from "@angular/material/icon";
 import {PostListComponent} from './posts/post-list/post-list.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {
-  MatAccordion,
-  MatExpansionPanel,
-  MatExpansionPanelHeader,
-  MatExpansionPanelTitle
-} from "@angular/material/expansion";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {LoginComponent} from './auth/login/login.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {AuthInterceptor} from "./auth/auth-interceptor";
 import {ErrorInterceptor} from "./error-nterceptor";
-import {MatDialogModule} from '@angular/material/dialog';
 import {ErrorComponent} from "./errors/error.component";
+import {AngularMaterialModule} from "./angular-material.module";
 
 @NgModule({
   declarations: [
@@ -47,24 +31,8 @@ import {ErrorComponent} from "./errors/error.component";
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    MatFormField,
-    MatInput,
-    MatCard,
-    MatButton,
-    MatToolbar,
-    MatIcon,
-    MatAccordion,
-    MatExpansionPanel,
-    MatExpansionPanelHeader,
-    MatExpansionPanelTitle,
-    MatFormFieldModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
     HttpClientModule,
-    MatAnchor,
-    MatCardFooter,
-    MatDialogModule,
+    AngularMaterialModule,
   ],
   providers: [
     provideAnimationsAsync(),
