d852b04892af72e2c8402667f5900081280950e2
andreymyqa <andrey@mytime.com>
2024-07-13 20:23:16 -0700

    11_Optimizations_132

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,42 +1,19 @@
 const express = require("express");
 const PostsController = require("../controllers/posts")
-
-const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
+const extractFile= require("../middleware/file");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  'image/jpg': "jpg",
-  'image/png': "png",
-  'image/jpeg': "jpeg"
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("File Type is invalid");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: function (req, file, cb) {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, `${name}-${Date.now().toString()}.${ext}`);
-  }
-})
-
 router.post( "",
   checkAuth,
-  multer({storage: storage}).single("image"),
+  extractFile,
   PostsController.createPost
 );
 
 router.put("/:id",
   checkAuth,
-  multer({storage: storage}).single("image"),
+  extractFile,
   PostsController.updatePost
 );
 
