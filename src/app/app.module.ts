7f05554ba34ed3e08224fb93a3c5d7b073b6a532
andreymyqa <andrey@mytime.com>
2024-07-13 21:56:14 -0700

    11_Optimizations_136

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,26 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {PostModule} from "./posts/post.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
 import {HeaderComponent} from './header/header/header.component';
-import {LoginComponent} from './auth/login/login.component';
-import {SignupComponent} from './auth/signup/signup.component';
 import {AuthInterceptor} from "./auth/auth-interceptor";
 import {ErrorInterceptor} from "./error-nterceptor";
 import {ErrorComponent} from "./errors/error.component";
 import {AngularMaterialModule} from "./angular-material.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
     BrowserModule,
     PostModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    AuthModule,
   ],
   providers: [
     provideAnimationsAsync(),
