8e469dbaad7f11eb2bd0ac872d2621e32ef70579
andreymyqa <andrey@mytime.com>
2024-08-04 15:11:20 -0700

    update for web deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'https://s.calinker.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 
 
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://s.calinker.com/api'
 };
